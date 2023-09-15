2c87cd972a342882fbc2f8653b42b46f0335a47c
OliverNasr <oliverfero65@gmail.com>
2023-09-14 17:01:54 -0700

    iloveu

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,15 +4,12 @@
 * Return: 0
 */
 int main(void)
-{
+{	
 	int i;
-	printf("%dInfinite loop incoming :(\n");
+
+	printf("Infinite loop incoming :(\n");
 	i = 0;
-/*while (i < 10)*/
-/*{*/
-/*putchar(i);*/
-/*}*/
-	printf("%dInfinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
 
